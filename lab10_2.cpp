5de05eab558aa75803535f798daa606b8758c02e
Tanathon801 <aumatomic@gmail.com>
2024-12-20 16:23:40 +0700

    yes

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -4,11 +4,20 @@
 using namespace std;
 
 int main (){
+	string x;
 	ifstream source;
 	ofstream dest;
 	source.open("cheerbook.txt") ;
 	dest.open("cheerbook_copy.txt");
-	
+	dest << "-------------------- BOOM ---------------------\n";
+	while(getline(source,x)){
+		dest << x <<"\n";
+
+	}
+	dest << "-------------------- HA!! ---------------------";
+
+
+
 	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
 	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
 
